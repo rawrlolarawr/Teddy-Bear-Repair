d8a8340aa745b3e3fa4efb6b155123a9abd28fac
christopherc0825 <christopherc0825@gmail.com>
2020-06-08 17:08:06 -0400

    adding part edit

--- app/controllers/part_controller.rb
+++ app/controllers/part_controller.rb
@@ -3,4 +3,17 @@ class PartController < ApplicationController
         @part = Part.find_by_id(params[:id])
         erb :'parts/show'
     end
+
+    get "/part/:id/edit" do
+        @part = Part.find_by_id(params[:id])
+        erb :'parts/edit'
+    end
+
+    patch "/part/:id/edit" do
+        @part = Part.find_by_id(params[:id])
+        if params[:name] != @part.name && params[:price] != @part.price && params[:ordered] != @part.ordered
+            @part.update(params[:part])
+        end
+        redirect "/part/#{@part.id}"
+    end
 end
