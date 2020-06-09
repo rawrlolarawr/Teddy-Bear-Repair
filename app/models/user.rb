b2eefc9621ed22c783693edb8a33c4df1bf60307
christopherc0825 <christopherc0825@gmail.com>
2020-06-09 16:56:28 -0400

    adding validation to Customer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
     has_many :customers
     has_many :devices, through: :customers
     has_many :parts, through: :devices
+    
     has_secure_password
+    validates :username, uniqueness: true
+    validates :username, presence: true
 end
