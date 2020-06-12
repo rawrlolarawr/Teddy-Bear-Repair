d7a26147684be2eb2058d5cd05cd52d1fbc4bd98
christopherc0825 <christopherc0825@gmail.com>
2020-06-12 15:37:04 -0400

    adding db files

---
+++ db/schema.rb
@@ -0,0 +1,44 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_05_21_220742) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.integer "user_id"
+  end
+
+  create_table "devices", force: :cascade do |t|
+    t.string "make"
+    t.string "model"
+    t.string "issue"
+    t.integer "hours_of_labor"
+    t.string "completed"
+    t.string "received"
+    t.decimal "total_price"
+    t.integer "customer_id"
+  end
+
+  create_table "parts", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.string "ordered"
+    t.integer "device_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
+
+end
