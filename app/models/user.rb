e2b17edd3260edc14aded254fa8ea1ab90cf267a
christopherc0825 <christopherc0825@gmail.com>
2020-06-12 12:00:38 -0400

    removing case sensitivity from username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :username, uniqueness: true
     validates :username, presence: true
+    validates :username, case_sensitive: false
 end
